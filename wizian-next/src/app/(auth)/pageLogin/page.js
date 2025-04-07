036757127d8ccb075ae2a08837ee9a3256bc0a97
이혜미 <svsvdvdv@naver.com>
2025-04-07 13:51:39 +0900

    로그인폼 회원가입 버튼 추가

--- wizian-next/src/app/(auth)/pageLogin/page.js
+++ wizian-next/src/app/(auth)/pageLogin/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import "@/app/(app)/pagelogin.css"
+import Link from "next/link";
 
 const PageLogin = () => {
     const [activeTab, setActiveTab] = useState("student");
@@ -12,7 +13,7 @@ const PageLogin = () => {
                     <div className="content">
                         <div className="header text-center">
                             <div className="logo mb-4">
-                                <img src="/assets/img/academiLogo_login.jpg" alt="Klorofil Logo" />
+                                <img src="/assets/img/academiLogo_login.jpg" alt="academi Logo" />
                             </div>
                             <div className="tab-buttons flex justify-center mb-4">
                                 <button
@@ -37,7 +38,7 @@ const PageLogin = () => {
                                     type="email"
                                     className="form-control"
                                     id="signin-email"
-                                    placeholder={activeTab === "student" ? "학생 이메일" : "강사 이메일"}
+                                    placeholder={activeTab === "student" ? "학생 아이디" : "강사 아이디"}
                                 />
                             </div>
                             <div className="form-group">
@@ -51,9 +52,16 @@ const PageLogin = () => {
                             </div>
                             <div className="form-group clearfix">
                                 <label className="fancy-checkbox element-left">
-                                    <input type="checkbox" />
+                                    <input type="checkbox"/>
                                     <span>기억하기</span>
                                 </label>
+                                {/*  회원가입 버튼은 학생 탭일 때만 보임 */}
+                                {activeTab === "student" && (
+                                    <span className="helper-text S-signup">
+                                        <Link href="/signup"> 회원가입</Link>
+                                        <i className="fa fa-paper-plane-o"></i>
+                                    </span>
+                                )}
                             </div>
 
                             <button type="submit" className="btn btn-login btn-lg btn-block">
@@ -63,8 +71,10 @@ const PageLogin = () => {
                             {/* 학생 로그인 탭일 때만 보이도록 조건부 렌더링 */}
                             {activeTab === "student" && (
                                 <>
-                                    <button type="button" className="btn btn-kakao btn-lg btn-block">Kakao 계정으로 로그인</button>
-                                    <button type="button" className="btn btn-google btn-lg btn-block">Google 계정으로 로그인</button>
+                                    <button type="button" className="btn btn-kakao btn-lg btn-block">Kakao 계정으로 로그인
+                                    </button>
+                                    <button type="button" className="btn btn-google btn-lg btn-block">Google 계정으로 로그인
+                                    </button>
                                 </>
                             )}
 
